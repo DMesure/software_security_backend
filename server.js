1648ef1d60d84a0ae8ae7d6ef57802505e247807
DMesure <dmesure98@gmail.com>
2020-12-30 10:39:17 +0100

    access control methods added

--- server.js
+++ server.js
@@ -28,8 +28,9 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, "
     );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 
